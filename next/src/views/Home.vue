b0d2295b8359d60ee3332dbb3c57f8852f62af63
GanJL <jianlin.gan.2020@scis.smu.edu.sg>
2021-10-25 13:54:33 +0800

    moved sign out to profile page

--- next/src/views/Home.vue
+++ next/src/views/Home.vue
@@ -131,14 +131,7 @@
 
 
 
-      <!-- <div>
-           <h1>Welcome {{ user?.email }}!</h1>
-      </div>
 
-      <div> -->
-          <!-- The code below shows error but it should still work -->
-          <!-- <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @click="signOutUser">Sign Out</button>
-      </div> -->
 
 
 
@@ -149,33 +142,7 @@
 
 </template>
 
-<script>
-import { getAuth, signOut } from 'firebase/auth'
-import { useAuthState } from '../firebase/firebase'
-import { useRouter } from 'vue-router'
 
-
-export default {
-  name: 'Home',
-  components: {
-  },
-  setup() {
-    const { user } = useAuthState()
-    const auth = getAuth()
-    const router = useRouter()
-
-    const signOutUser = async () => {
-      try {
-        await signOut(auth)
-        router.push('/login')
-      } catch (e) {
-        alert(e.message)
-      }
-    }
-    return { user, signOutUser }
-    }, 
-}
-</script>
 
 
 
--- next/src/views/Profile.vue
+++ next/src/views/Profile.vue
@@ -3,4 +3,39 @@
   <div class="profile">
     <h1 class="mt-7">This is a profile page</h1>
   </div>
+
+  <div>
+      <h1>Welcome {{ user?.email }}!</h1>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @click="signOutUser">Sign Out</button>
+  </div>
+
+
 </template>
+
+<script>
+import { getAuth, signOut } from 'firebase/auth'
+import { useAuthState } from '../firebase/firebase'
+import { useRouter } from 'vue-router'
+
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  setup() {
+    const { user } = useAuthState()
+    const auth = getAuth()
+    const router = useRouter()
+
+    const signOutUser = async () => {
+      try {
+        await signOut(auth)
+        router.push('/login')
+      } catch (e) {
+        alert(e.message)
+      }
+    }
+    return { user, signOutUser }
+    }, 
+}
+</script>
