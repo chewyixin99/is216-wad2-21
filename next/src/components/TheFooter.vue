4d861eafe110a38d9ab2272edf4474aba0d13d95
joellim2020 <joel.lim.2020@scis.smu.edu.sg>
2021-10-26 15:11:51 +0800

    fixed onboarding, add assets

--- next/src/components/TheFooter.vue
+++ next/src/components/TheFooter.vue
@@ -1,9 +1,14 @@
 <template>
-  
+  <div if="footer" class="flex items-center justify-between flex-wrap p-6 bg-purple-900 bottom-0">
+    <p class="text-center text-gray-500 text-xs">
+      &copy;Project Next. All rights reserved.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
+    name: "footer"
 
 }
 </script>
