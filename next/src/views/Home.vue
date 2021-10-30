f0f08cf049102d43d57a19a18e24e978bac96071
Chew Yi Xin <chewyixin1999@gmail.com>
2021-10-31 00:16:02 +0800

    responsive grid added for home

--- next/src/views/Home.vue
+++ next/src/views/Home.vue
@@ -1,149 +1,149 @@
 <template>
 
-    <div class="container mx-auto my-3 flex items-center justify-center">
-      
-      <div class="grid grid-cols-1 lg:grid-cols-2">
-        <div class="col-span-2">
-          <!-- Searchbar Here
-          <div class="search">
-          </div> -->
-          <form class="m-5" action="">
-            <input type="text" class="shadow appearance-none border rounded w-10/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="court-search" placeholder="Search for a court">
-            <button class="search-button hover:bg-yellow-300 text-gray-700 font-bold mx-3 py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-              Search
-            </button>
-          </form>
-        </div>
-       
-
-        <div>
-          <div class="primary-title">
-            Bookmarks
-          </div>
-          <div class="bookmarks bg-gray-800 text-white rounded-md py-2 px-4 mr-10">
-            <ul>
-              <li>
-                <div class="venue-name">
-                  Heartbeat @ Bedok ActiveSG Swimming Complex
-                </div>
-                <div class="venue-address">
-                  3 Bedok North Street 2, Singapore 469643
-                </div>
-              </li>
-
-              <li>
-                <div class="venue-name">
-                  Heartbeat @ Bedok ActiveSG Swimming Complex
-                </div>
-                <div class="venue-address">
-                  3 Bedok North Street 2, Singapore 469643
-                </div>
-              </li>
-
-              <li>
-                <div class="venue-name">
-                  Heartbeat @ Bedok ActiveSG Swimming Complex
-                </div>
-                <div class="venue-address">
-                  3 Bedok North Street 2, Singapore 469643
-                </div>
-              </li>
-
-              <li>
-                <div class="venue-name">
-                  Heartbeat @ Bedok ActiveSG Swimming Complex
-                </div>
-                <div class="venue-address">
-                  3 Bedok North Street 2, Singapore 469643
-                </div>
-              </li>
-
-              <li>
-                <div class="venue-name">
-                  Heartbeat @ Bedok ActiveSG Swimming Complex
-                </div>
-                <div class="venue-address">
-                  3 Bedok North Street 2, Singapore 469643
-                </div>
-              </li>
-
-              <li>
-                <div class="venue-name">
-                  Heartbeat @ Bedok ActiveSG Swimming Complex
-                </div>
-                <div class="venue-address">
-                  3 Bedok North Street 2, Singapore 469643
-                </div>
-              </li>
-              
-            </ul>
-          </div>
-        </div>
-
-        <div class="row-span-2">
-          <div class="primary-title">
-            Near You
-          </div>
-          <div class="near-you">
-
-          </div>
-        </div>
-
-        <div>
-          <div class="primary-title">
-            Recently Played
-          </div>
-          <div class="recently-played">
-            <div class="player-image">
-              <img src="https://t3.ftcdn.net/jpg/02/22/85/16/240_F_222851624_jfoMGbJxwRi5AWGdPgXKSABMnzCQo9RN.jpg" alt="">
-            </div>
-
-            <div class="player-no-image">
-              YX
-            </div>
-
-            <div class="player-image">
-              <img src="https://www.w3schools.com/css/paris.jpg" alt="">
-            </div>
-
-            <div class="player-image">
-              <img src="https://t3.ftcdn.net/jpg/04/40/13/28/240_F_440132822_ZdGM5QYX399ieufL0D0T0r0ILXdHiSv7.jpg" alt="">
-            </div>
-            
-            <div class="player-image">
-              <img src="https://t3.ftcdn.net/jpg/04/55/75/46/240_F_455754611_8eowWGUS88rIH74lyLaEgAHim7XPc2Os.jpg" alt="">
-            </div>
-
-            <div class="player-no-image">
-              KW
-            </div>
-
-            <div class="player-no-image">
-              JL
-            </div>
-
-          </div>
-        </div>
-
-        
-      </div>
+<div class="container mx-auto my-3 flex items-center justify-center">
 
 
+  <div class="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2">
 
+    <!-- Search  -->
+    <div class="col-span-2" > 
 
-
-
-
-
-
-     
+      <form class="m-5" action="">
+        <input type="text" class="shadow appearance-none border rounded w-10/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="court-search" placeholder="Search for a court">
+        <button class="search-button hover:bg-yellow-300 text-gray-700 font-bold mx-3 py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          Search
+        </button>
+      </form>
 
     </div>
 
+    <!-- Bookmarks -->
+    <div class="col-span-2 md:col-span-1">
+
+      <div class="primary-title">
+        Bookmarks
+      </div>
+      <div class="bookmarks bg-gray-800 text-white rounded-md py-2 px-4 mr-10">
+        <ul>
+          <li>
+            <div class="venue-name">
+              Heartbeat @ Bedok ActiveSG Swimming Complex
+            </div>
+            <div class="venue-address">
+              3 Bedok North Street 2, Singapore 469643
+            </div>
+          </li>
+
+          <li>
+            <div class="venue-name">
+              Heartbeat @ Bedok ActiveSG Swimming Complex
+            </div>
+            <div class="venue-address">
+              3 Bedok North Street 2, Singapore 469643
+            </div>
+          </li>
+
+          <li>
+            <div class="venue-name">
+              Heartbeat @ Bedok ActiveSG Swimming Complex
+            </div>
+            <div class="venue-address">
+              3 Bedok North Street 2, Singapore 469643
+            </div>
+          </li>
+
+          <li>
+            <div class="venue-name">
+              Heartbeat @ Bedok ActiveSG Swimming Complex
+            </div>
+            <div class="venue-address">
+              3 Bedok North Street 2, Singapore 469643
+            </div>
+          </li>
+
+          <li>
+            <div class="venue-name">
+              Heartbeat @ Bedok ActiveSG Swimming Complex
+            </div>
+            <div class="venue-address">
+              3 Bedok North Street 2, Singapore 469643
+            </div>
+          </li>
+
+          <li>
+            <div class="venue-name">
+              Heartbeat @ Bedok ActiveSG Swimming Complex
+            </div>
+            <div class="venue-address">
+              3 Bedok North Street 2, Singapore 469643
+            </div>
+          </li>
+          
+        </ul>
+      </div>
+
+    </div>
+
+
+    <!-- Near You -->
+    <div class="col-span-2 md:col-span-1 row-span-2">
+
+      <div class="primary-title">
+        Near You
+      </div>
+      <div class="near-you"></div>
+    
+    </div>
+
+
+    <!-- Recently Played -->
+    <div class="col-span-2 md:col-span-1">
+      <div class="primary-title">
+        Recently Played
+      </div>
+      <div class="recently-played">
+        <div class="player-image">
+          <img src="https://t3.ftcdn.net/jpg/02/22/85/16/240_F_222851624_jfoMGbJxwRi5AWGdPgXKSABMnzCQo9RN.jpg" alt="">
+        </div>
+
+        <div class="player-no-image">
+          YX
+        </div>
+
+        <div class="player-image">
+          <img src="https://www.w3schools.com/css/paris.jpg" alt="">
+        </div>
+
+        <div class="player-image">
+          <img src="https://t3.ftcdn.net/jpg/04/40/13/28/240_F_440132822_ZdGM5QYX399ieufL0D0T0r0ILXdHiSv7.jpg" alt="">
+        </div>
+        
+        <div class="player-image">
+          <img src="https://t3.ftcdn.net/jpg/04/55/75/46/240_F_455754611_8eowWGUS88rIH74lyLaEgAHim7XPc2Os.jpg" alt="">
+        </div>
+
+        <div class="player-no-image">
+          KW
+        </div>
+
+        <div class="player-no-image">
+          JL
+        </div>
+
+      </div>
+
+    </div>
+
+
+
+  </div> <!-- grid -->
+
+
+</div>
+
+
+
 </template>
-
-
-
 
 
 <style scoped>
@@ -163,8 +163,8 @@
 
 .near-you {
   background-color: white;
-  height: 500px;
-  width: 500px;
+  height: 400px;
+  width: 400px;
 }
 
 .recently-played {
@@ -206,19 +206,6 @@
 .venue-address {
   font-size: 12px;
   color: #949494;
-}
-
-.venue-address:after {
-  background: white;
-  width: 100%;
-  height: 5px;
-
-}
-
-.search {
-  background-color: white;
-  height: 40px;
-  width: 100%
 }
 
 </style>
