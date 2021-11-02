5aee323a5ccf1deb838b615b2323b2b5b2fc8e2b
joellim2020 <joel.lim.2020@scis.smu.edu.sg>
2021-11-03 00:07:47 +0800

    style; forgotpw, editprofile, profile

--- next/src/views/ForgotPW.vue
+++ next/src/views/ForgotPW.vue
@@ -1,23 +1,34 @@
 <template>
-<div class="w-full max-w-xs">
-    <div class="px-8 pt-6 text-gray-700 text-lg font-bold mb-2 justify-items-center">Reset Password</div>
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="email" >
-            Enter valid email address to reset 
-        </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" v-model="email">
+<div class="container max-w-md mx-auto">
+    <!-- HEADER -->
+    <div class="secondary-gold-title text-center mt-6 mb-3 justify-items-center">
+        Password Reset
+    </div>
+
+    <div class="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="text-white mb-4">
+            Enter your email address and we'll send you a link to reset your password.
         </div>
-        <div class="flex items-center justify-between">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" @click.prevent="reset">
-            Submit
-        </button>
-        
-        </div>
-    </form>
-    <p class="text-center text-gray-500 text-xs">
-        &copy;Project Next. All rights reserved.
-    </p>
+
+        <hr class="mb-4">
+
+        <form>
+            <!-- EMAIL INPUT -->
+            <div class="mb-6">
+                <label class="block text-white text-sm font-bold mb-2" for="email" >
+                    ENTER EMAIL 
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" v-model="email">
+            </div>
+            <!-- SUBMIT BUTTON -->
+            <div class="text-center">
+                <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded w-36" @click.prevent="reset">
+                SUBMIT
+            </button>
+            
+            </div>
+        </form>
+    </div>
 </div>
 </template>
 
