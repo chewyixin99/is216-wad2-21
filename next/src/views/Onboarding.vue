c674c4607adae640c5eaa5f550e2020a5e0ef46e
joellim2020 <joel.lim.2020@scis.smu.edu.sg>
2021-11-02 17:15:28 +0800

    updated style

--- next/src/views/Onboarding.vue
+++ next/src/views/Onboarding.vue
@@ -4,51 +4,59 @@
 
         <div class="grid grid-cols-1 mx-6">
             <form>
-                <div class="bg-white shadow-md rounded px-8 py-6 my-6">
+                <div class="bg-gray-800 shadow-md rounded px-8 py-6 my-6">
                     
                         <!-- FIRST AND LAST NAME -->
                         <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
                             <div>
-                                <label class="block text-gray-700 text-sm font-bold mb-2" for="firstName">
-                                First Name
+                                <label class="block text-white text-sm font-bold mb-2" for="firstName">
+                                FIRST NAME
                                 </label>
                                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="firstName" type="text" v-model="firstName">
                             </div>
                             <div>
-                                <label class="block text-gray-700 text-sm font-bold mb-2" for="lastName">
-                                Last Name
+                                <label class="block text-white text-sm font-bold mb-2" for="lastName">
+                                LAST NAME
                                 </label>
                                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="lastName" type="text" v-model="lastName">
                             </div>
                         </div>
 
+                        <!-- EMAIL -->
+                        <div class="mb-4">
+                            <label class="block text-white text-sm font-bold mb-2" for="email">
+                                EMAIL
+                            </label>
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" name="email" v-model="email">
+                        </div>
+
                         <!-- EXPERIENCE -->
                         <div class="mb-4">
-                            <label class="block text-gray-700 text-sm font-bold mb-2" for="experience">
-                                Experience
+                            <label class="block text-white text-sm font-bold mb-2" for="experience">
+                                EXPERIENCE
                             </label>
 
                             <select class="form-select mt-1 block w-full shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="experience" name="experience" v-model="experience">
-                                <option>Recreational</option>
-                                <option>Intermediate</option>
-                                <option>Competitive</option>
+                                <option>RECREATIONAL</option>
+                                <option>INTERMEDIATE</option>
+                                <option>COMPETITIVE</option>
                             </select>
                         </div>
 
                         <!-- FAVOURITE PLAYER -->
                         <div class="mb-4">
-                            <label class="block text-gray-700 text-sm font-bold mb-2" for="favPlayer">
-                                Favourite Player
+                            <label class="block text-white text-sm font-bold mb-2" for="favPlayer">
+                                FAVOURITE PLAYERS
                             </label>
-                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="favPlayer" type="text" name="favPlayer" v-model="favPlayer">
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="favPlayer" type="text" name="favPlayer" v-model="favPlayer" placeholder="FAVOURITE PLAYERS">
                         </div>
 
                         <!-- FAVOURITE TEAM -->
                         <div class="mb-4">
-                            <label class="block text-gray-700 text-sm font-bold mb-2" for="favTeam">
-                                Favourite Teams
+                            <label class="block text-white text-sm font-bold mb-2" for="favTeam">
+                                FAVOURITE TEAMS
                             </label>
-                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="favTeam" type="text" name="favTeam" v-model="favTeam">
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="favTeam" type="text" name="favTeam" v-model="favTeam" placeholder="FAVOURITE TEAMS">
                         </div>
 
                 </div>
@@ -56,7 +64,7 @@
                 <!-- SUBMIT BUTTON -->
                 <div class="text-center mb-4">
                     <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded w-64" @click.prevent="onboarding">
-                    Let's Play
+                    SAVE
                     </button>
                 </div>
 
@@ -80,9 +88,10 @@ export default {
     name: "Onboarding",
 data(){
     return{
-        // retrieve first and last name from firestore
+        // retrieve first, last name and email from firestore
         firstName: this.firstName,
         lastName: this.lastName,
+        email: this.email,
         experience: this.experience,
         favTeam: "",
         favPlayer: "",
@@ -102,6 +111,7 @@ data(){
                 dataBase.set({
                     firstName: this.firstName,
                     lastName: this.lastName,
+                    email: this.email,
                     experience: this.experience,
                     favPlayer: this.favPlayer,
                     favTeam: this.favTeam,
