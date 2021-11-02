f35c8b4c2029ad79c188aca7387177867ee011ab
Chew Yi Xin <chewyixin1999@gmail.com>
2021-11-02 22:05:39 +0800

    refactor maps

--- next/src/components/googleMaps/useGeolocation.js
+++ next/src/components/googleMaps/useGeolocation.js
@@ -1,16 +1,19 @@
 import { onUnmounted, onMounted, ref } from "vue";
 
 export function useGeolocation() {
-    const coords = ref({latitude: 0, longitude: 0})
 
+    const coords = ref({latitude: 1.353, longitude: 103.867})
     const isSupported = 'navigator' in window && 'geolocation' in navigator
-
+    
     let watcher = null
-    onMounted(() => {
-        if (isSupported)
+    onMounted( ()  => {
+        if (isSupported) {
             watcher = navigator.geolocation.watchPosition(
-                position => (coords.value = position.coords)
+                position => {
+                    coords.value = position.coords
+                }
             )
+        }
     })
 
     onUnmounted(() => {
@@ -18,6 +21,25 @@ export function useGeolocation() {
             navigator.geolocation.clearWatch(watcher)
     })
 
+
     return { coords, isSupported }
+
+}
+
+export function findNearbyCourts(lat, lng) {
+    let baseUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?`
+    let location = `${lat}%2c${lng}`
+    let radius = `1500`
+    let keyword = `basketball courts`
+    let API_KEY = `AIzaSyB0y4bi5X2uc_EZGF8yE-GIc_09jd9rwRg`
+
+    let final_url = `${baseUrl}location=${location}&radius=${radius}&keyword=${keyword}&key=${API_KEY}`
+
+    console.log(final_url)
+    // axios.get(final_url).then(r => {
+    //     console.log(r.data)
+    // }).catch(e => {
+    //     console.log(e.memssage)
+    // })
 
 }
--- next/src/main.js
+++ next/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
+// import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 import './assets/style.css'
 
+
 createApp(App).use(router).mount('#app')
+// App.config.globalProperties.axios=axios
