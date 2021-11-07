eefeebf085ad33859de9e2f25361aeb70f457d79
joellim2020 <joel.lim.2020@scis.smu.edu.sg>
2021-11-07 23:56:32 +0800

    home style

--- next/src/views/Home.vue
+++ next/src/views/Home.vue
@@ -5,15 +5,16 @@
 
 <div class="container max-w-5xl mx-auto">
 
-  <div class="grid grid-cols-2  lg:grid-cols-2">
+  <div class="mx-6">
 
     <!-- Search  -->
-    <div class="col-span-2 " > 
+    <div class="mt-6" > 
 
-      <form class="m-5" action="">
-        <input type="text" class=" search-bar shadow appearance-none border rounded w-10/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="court-search" placeholder="Search for a location, i.e., Serangoon Nex, Pasir Ris block 134  " v-model="searchInput">
-        <button v-on:click="searchForNearbyCourts()" class="search-button bg-yellow-400 hover:bg-yellow-300 text-gray-700 font-bold mx-3 py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-          Search
+      <form class="w-full md:w-10/12 flex space-x-2 mx-auto" action="">
+        <input type="text" class=" search-bar shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="court-search" placeholder="Search for a location, i.e., Serangoon Nex, Pasir Ris block 134  " v-model="searchInput">
+
+        <button v-on:click="searchForNearbyCourts()" class="search-button bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          SEARCH
         </button>
       </form>
 
@@ -23,109 +24,119 @@
       {{ $store.state.user }}
     </div> -->
 
-    <!-- Bookmarks -->
-    <div class="col-span-2 lg:col-span-1">
+    <div class="grid grid-cols-1 md:grid-cols-2 mt-4 gap-4">
 
-      <div class="primary-title">
-        Bookmarks
+      <!-- Bookmarks -->
+      <div class="col-span-1">
+
+        <div class="primary-title">
+          Bookmarks
+        </div>
+
+        <div v-if="currentBookmarks.length < 1" >
+          <span class="secondary-title">You currently have no bookmarks!</span><br/>
+          <button 
+          class="search-button bg-yellow-500 hover:bg-yellow-700 text-white font-bold my-3 py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+          v-on:click="findNearbyCourts(currPos.lat, currPos.lng)"
+          > Find A Court Near Me
+          </button>
+        </div>
+
+        <div v-else class="bookmarks bg-gray-800 text-white rounded-md py-2 px-4">
+
+          <ul v-for="b in currentBookmarks" :key="b">
+            <li>
+              <a href="/court" v-on:click="updateSelectedCourt(b)">
+                <div class="venue-name">
+                    {{ b.name }}
+                </div>
+                <div class="venue-address">
+                  {{ b.vicinity }}
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+
       </div>
 
-      <div v-if="currentBookmarks.length < 1" >
-        <span class="secondary-title">You currently have no bookmarks!</span><br/>
-        <button 
-        class="search-button bg-yellow-400 hover:bg-yellow-300 text-gray-700 font-bold my-3 py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
-        v-on:click="findNearbyCourts(currPos.lat, currPos.lng)"
-        > Find A Court Near Me
-        </button>
-      </div>
 
-      <div v-else class="bookmarks bg-gray-800 text-white rounded-md py-2 px-4 mr-10">
+      <!-- Near You -->
+      <div class="col-span-1 row-span-1 md:row-span-2" >
 
-        <ul v-for="b in currentBookmarks" :key="b">
-          <li>
-            <a href="/court" v-on:click="updateSelectedCourt(b)">
-              <div class="venue-name">
-                  {{ b.name }}
-              </div>
-              <div class="venue-address">
-                {{ b.vicinity }}
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
+        <div class="flex justify-between">
+          <div class="primary-title">
+            Near You
+          </div>
+          <button 
+          class="search-button bg-blue-500 hover:bg-blue-700 text-white text-xs font-bold my-3 px-2 rounded focus:outline-none focus:shadow-outline" 
+          v-on:click="findNearbyCourts(currPos.lat, currPos.lng)">
+            REFRESH
+          </button>
+        </div>
 
-    </div>
+        <div id="mapDiv" class="mapDiv rounded" ref="mapDiv" ></div> 
 
+        <!-- <button 
+        class="search-button bg-yellow-500 hover:bg-yellow-700 text-white font-bold my-3 py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+        v-on:click="findNearbyCourts(currPos.lat, currPos.lng)">
+          COURTS NEAR ME
+        </button> -->
 
-    <!-- Near You -->
-    <div class="col-span-2 lg:col-span-1 row-span-1 lg:row-span-2" >
-
-      <div class="primary-title">
-        Near You
-      </div>
-      <div id="mapDiv" class="mapDiv" ref="mapDiv" ></div> 
-
-      <button 
-      class="search-button bg-yellow-400 hover:bg-yellow-300 text-gray-700 font-bold my-3 py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
-      v-on:click="findNearbyCourts(currPos.lat, currPos.lng)"
-      > Courts Near Me
-      </button>
-
+        
+        <!-- <div>
+          <h4>Your position</h4>
+          lat : {{ currPos.lat }}
+          lng: {{ currPos.lng }}
+        </div>
+        <div>
+          <h4>Clicked Position</h4>
+          <span v-if="otherPos">
+            lat: {{ otherPos.lat }}
+            lng: {{ otherPos.lng }}
+          </span>
+          <span v-else> 
+            Click the map to select a position
+          </span>
+        </div> -->
       
-      <!-- <div>
-        <h4>Your position</h4>
-        lat : {{ currPos.lat }}
-        lng: {{ currPos.lng }}
-      </div>
-      <div>
-        <h4>Clicked Position</h4>
-        <span v-if="otherPos">
-          lat: {{ otherPos.lat }}
-          lng: {{ otherPos.lng }}
-        </span>
-        <span v-else> 
-          Click the map to select a position
-        </span>
-      </div> -->
-    
-    </div>
-
-
-    <!-- Recently Played -->
-    <div class="col-span-2 lg:col-span-1">
-      <div class="primary-title">
-        Recently Played
-      </div>
-      <div class="recently-played">
-
-        
-        <ul class="recently-played mr-4">
-          <li v-for="p in  recentlyPlayed" :key="p">
-            <a href="/publicuser" v-on:click="updateSelectedProfile(p)">
-              <span v-if="p.profileImg.length > 0" >
-                <div class="player-image">
-                  <img v-bind:src="p.profileImg" alt="">
-                </div>
-              </span>
-
-              <span v-else>
-                <div class="player-no-image">
-                  {{p.firstName.charAt(0)}}{{p.lastName.charAt(0)}}
-                </div>
-              </span>
-            </a>
-          </li>          
-        </ul>
-        
-
       </div>
 
-    </div>
 
+      <!-- Recently Played -->
+      <div class="col-span-1">
+        <div class="primary-title">
+          Recently Played
+        </div>
+        <div class="recently-played">
 
+          
+          <ul class="recently-played mr-4">
+            <li v-for="p in  recentlyPlayed" :key="p">
+              <a href="/publicuser" v-on:click="updateSelectedProfile(p)">
+                <span v-if="p.profileImg.length > 0" >
+                  <div class="player-image">
+                    <img v-bind:src="p.profileImg" alt="">
+                  </div>
+                </span>
+
+                <span v-else>
+                  <div class="player-no-image">
+                    {{p.firstName.charAt(0)}}{{p.lastName.charAt(0)}}
+                  </div>
+                </span>
+              </a>
+            </li>          
+          </ul>
+          
+
+        </div>
+
+      </div>
 
   </div> <!-- grid -->
+
+  </div> <!-- end of container -->
 
 
 </div>
